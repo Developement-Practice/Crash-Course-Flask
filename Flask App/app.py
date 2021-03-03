30d6dbfe7187c8a6b03b708d27796411373a4e03
Kaustav Ghosh <teetangh@gmail.com>
2021-03-03 23:05:37 +0530

    Completed Flask App

--- Flask App/app.py
+++ Flask App/app.py
@@ -41,7 +41,6 @@ def index():
 
 @app.route('/posts',  methods=['GET', 'POST'])
 def posts():
-
     if request.method == "POST":
         post_title = request.form['title']
         post_content = request.form['content']
@@ -88,6 +87,23 @@ def edit(id):
         return redirect('/posts')
     else:
         return render_template('edit.html', post=post)
+
+
+@app.route('/posts/new', methods=["GET", "POST"])
+def new_post():
+
+    if request.method == "POST":
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+
+        new_post = BlogPost(
+            title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template("new_post.html")
 
 
 if __name__ == "__main__":
